b56c09d6a156b4ad2116631a809b4a3030c09a74
Luana Ataide <ataideacademic@gmail.com>
2024-12-16 11:03:12 -0300

    Delete seller

--- SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -18,5 +18,18 @@ namespace SalesWebMvc.Services
             return _context.Department.OrderBy(x => x.Name).ToList();
         }
 
+        public Seller FindById (int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove (int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
+
+
     }
 }
